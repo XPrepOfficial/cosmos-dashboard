38fff1fc547c900a4a7e7f36d9b4b6734e01c8c5
Siddharth Jain <siddharth.jain@classplus.co>
2023-05-03 11:59:08 +0530

    adding skeleton for cards component

--- src/Screens/Dashboard/Components/InfoCard/index.jsx
+++ src/Screens/Dashboard/Components/InfoCard/index.jsx
@@ -1,43 +1,51 @@
-import { Badge, Space } from "antd";
-import { Card } from "antd";
+import { Badge, Skeleton, Space, Card } from "antd";
+import { DummyArray8, CardsColorMap } from "../../../../utils/helper";
 import "./InfoCard.css";
 
 const { Meta } = Card;
 
-const CardsColorMap = {
-  ENTRIES: "pink",
-  EXITS: "volcano",
-  "IN JOURNEY": "gold",
-  SENT: "lime",
-  DELIVERED: "orange",
-  CLICKS: "geekblue",
-  CONVERSIONS: "green",
-  REVENUE: "cyan",
+const SkeletonCard = () => {
+  return (
+    <div className="info-card-container">
+      {DummyArray8.map((ele) => (
+        <Card key={ele} style={{ width: 300, marginTop: 16 }}>
+          <Skeleton loading={true} avatar active>
+            <Meta title="" />
+          </Skeleton>
+        </Card>
+      ))}
+    </div>
+  );
 };
 
-const InfoCard = ({ journeyCardData }) => (
-  <div className="info-card-container">
-    {journeyCardData?.data.map((card) => (
-      <Card
-        key={card.cardName}
-        style={{ width: 300, marginTop: 16 }}
-        loading={journeyCardData?.isLoading}
-      >
-        <Meta
-          avatar={
-            <Space direction="vertical">
-              <Badge
-                color={CardsColorMap[card.cardName] || "black"}
-                size={240}
-              />
-            </Space>
-          }
-          title={card.cardName}
-          description={card.cardCount}
-        />
-      </Card>
-    ))}
-  </div>
-);
+const InfoCard = ({ journeyCardData }) => {
+  if (!journeyCardData?.data?.length) {
+    return <SkeletonCard />;
+  }
+  return (
+    <div className="info-card-container">
+      {journeyCardData?.data.map((card) => (
+        <Card
+          key={card.cardName}
+          style={{ width: 300, marginTop: 16 }}
+          loading={journeyCardData?.isLoading}
+        >
+          <Meta
+            avatar={
+              <Space direction="vertical">
+                <Badge
+                  color={CardsColorMap[card.cardName] || "black"}
+                  size={240}
+                />
+              </Space>
+            }
+            title={card.cardName}
+            description={card.cardCount}
+          />
+        </Card>
+      ))}
+    </div>
+  );
+};
 
 export default InfoCard;
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,19 @@
 export const StatusColorMap = {
-    UPCOMING: "#F7C144",
-    RUNNING: "#62D492",
-    STOPPED: "#E76D69",
-    DRAFT: "#94969E",
-}
+  UPCOMING: "#F7C144",
+  RUNNING: "#62D492",
+  STOPPED: "#E76D69",
+  DRAFT: "#94969E",
+};
+
+export const CardsColorMap = {
+  ENTRIES: "pink",
+  EXITS: "volcano",
+  "IN JOURNEY": "gold",
+  SENT: "lime",
+  DELIVERED: "orange",
+  CLICKS: "geekblue",
+  CONVERSIONS: "green",
+  REVENUE: "cyan",
+};
+
+export const DummyArray8 = [0, 1, 2, 3, 4, 5, 6, 7];
