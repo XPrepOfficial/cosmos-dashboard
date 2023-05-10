3bd1156b67900d49b0547a5b0d50892f080f1ee8
Siddharth Jain <siddharth.jain@classplus.co>
2023-05-10 16:07:48 +0530

    changing api

--- src/utils/api.constants.js
+++ src/utils/api.constants.js
@@ -1,13 +1,14 @@
 const urls = {
-  EXPORT_REPORT: "journey/reports",
-  FETCH_JOURNEY: "journey/list?limit={0}&offset={1}",
+  EXPORT_REPORT: "analytics-api/v1/journey/reports",
+  FETCH_JOURNEY: "analytics-api/v1/journey/list?limit={0}&offset={1}",
   FETCH_JOURNEY_CARD_DATA:
-    "journey/stats?journeyId={0}&startDate={1}&endDate={2}",
+    "analytics-api/v1/journey/stats?journeyId={0}&startDate={1}&endDate={2}",
   FETCH_CAMPAIGNS_TABLE_DATA:
-    "journey/campaigns/stats?journeyId={0}&startDate={1}&endDate={2}&&limit={3}&offset={4}",
-  SELECT_ORGS_LIST: "journey/orgs?journeyId={0}&limit={1}&offset={2}",
+    "analytics-api/v1/journey/campaigns/stats?journeyId={0}&startDate={1}&endDate={2}&&limit={3}&offset={4}",
+  SELECT_ORGS_LIST:
+    "analytics-api/v1/journey/orgs?journeyId={0}&limit={1}&offset={2}",
   SELECT_SEARCH_ORGS_LIST:
-    "journey/orgs?journeyId={0}&limit={1}&offset={2}&search={3}",
+    "analytics-api/v1/journey/orgs?journeyId={0}&limit={1}&offset={2}&search={3}",
 };
 
 export default (type, ...params) => {
