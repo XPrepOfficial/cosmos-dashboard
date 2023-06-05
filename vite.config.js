e40ecc2a5fe1aa1116500cc70de39097e1570cfa
Siddharth Jain <siddharth.jain@classplus.co>
2023-06-05 19:19:25 +0530

    test: build failed: changing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,8 @@
-// import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
-// import reactRefresh from "@vitejs/plugin-react-refresh";
+
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "Fragment",
   },
 });
