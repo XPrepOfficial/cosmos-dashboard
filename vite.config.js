bcdec9b3ba7d6eefc2a18b05c07ad04ead26d101
Siddharth Jain <siddharth.jain@classplus.co>
2023-06-08 12:51:57 +0530

    test: build failed: test45

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import fs from "node:fs";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import * as esbuild from "esbuild";
+import svgr from "vite-plugin-svgr";
+// import path from "node:path";
 
-// https://vitejs.dev/config/
+// js and jsx
+const sourceJSPattern = /\.(js|jsx)$/;
+// const rollupPlugin = (matchers) => ({
+//   name: "js-in-jsx",
+
+//   load(id) {
+//     if (matchers.some((matcher) => matcher.test(id))) {
+//       const file = fs.readFileSync(id, { encoding: "utf-8" });
+//       return esbuild.transformSync(file, { loader: "jsx" });
+//     }
+//   },
+// });
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr({ svgrOptions: { icon: true } })],
+  server: {
+    host: "localhost",
+    port: 3000,
+    strictPort: true,
+    open: true,
+  },
+  base: "",
+  build: {
+    rollupOptions: {
+      plugins: [react()],
+    },
+  },
+
+  optimizeDeps: {
+    enable: false,
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  esbuild: {
+    loader: "jsx",
+    include: [sourceJSPattern],
+    exclude: [],
+  },
+});
